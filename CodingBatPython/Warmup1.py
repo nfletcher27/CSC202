30646df17e1d57be793cb14d36edee7d20126eca
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-26 22:28:55 -0500

    Create Warmup1.py

---
+++ CodingBatPython/Warmup1.py
@@ -0,0 +1,18 @@
+# CodingBat problems
+# sleep_in
+def sleep_in(weekday, vacation):
+  
+  if vacation:
+    return True
+  elif weekday:
+    return False
+  else: 
+    return True
+
+# monkey_trouble
+def monkey_trouble(a_smile, b_smile):
+  
+  if a_smile == b_smile:
+    return True
+  else:
+    return False
