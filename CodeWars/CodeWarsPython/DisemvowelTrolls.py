12e382c545ba5a31d13f10bc5c5b04e4fb7fe383
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-06 01:21:26 -0500

    Create DisemvowelTrolls.py

---
+++ CodeWars/CodeWarsPython/DisemvowelTrolls.py
@@ -0,0 +1,14 @@
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+def disemvowel(string_):
+    
+    string_ = string_.replace("e", "")
+    string_ = string_.replace("a", "")
+    string_ = string_.replace("i", "")
+    string_ = string_.replace("o", "")
+    string_ = string_.replace("u", "")
+    string_ = string_.replace("E", "")
+    string_ = string_.replace("A", "")
+    string_ = string_.replace("I", "")
+    string_ = string_.replace("O", "")
+    string_ = string_.replace("U", "")
+    return string_
