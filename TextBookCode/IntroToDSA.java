93ace572e95f18f414e9c70e9a9d1c13b0bafb07
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-08 18:23:46 -0500

    Update IntroToDSA.java

--- TextBookCode/IntroToDSA.java
+++ TextBookCode/IntroToDSA.java
@@ -34,3 +34,23 @@ int[] intArray = new int[100]; // creates and assigns size to new array
 temp = intArray[3];
 intArray[7] = 66;
 
+class Person {
+  private String lastName;
+  private String firsteName;
+  private int age;
+  
+  public Person(String last, String first, int a) {
+    lastName = last;
+    firstName = first;
+    age = a;
+  }
+  
+  public void displayPerson() {
+    System.out.print("  Last name: " +lastName);
+    System.out.print(" First name:" + firstName);
+    System.out.println(", Age: " + age);
+  }
+  
+  public String getLast() {
+    return lastName;
+  }
