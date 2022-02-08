888b119702bf0637852192a7c82f3186794bda4a
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-07 23:31:46 -0500

    Create IntroToDSA.java

---
+++ TextBookCode/IntroToDSA.java
@@ -0,0 +1,20 @@
+// Chapter 1 of DSA
+class thermostat {
+  private float currentTemp();
+  private float desiredTemp();
+  
+  public void furnace_on() {
+    // method body for activity
+  }
+  
+  public void furnace_off() {
+    // method body for activity
+  }
+} // end class thermostat
+
+thermostat therm1; threrm2;
+
+therm1 = new thermostat();
+therm2 = new thermostat();
+
+therm2.furnace_on(); // accessing
