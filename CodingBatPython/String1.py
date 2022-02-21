b1fb9ae7b81bc6c22934792223800931608543ed
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-21 16:10:50 -0500

    Update String1.py

--- CodingBatPython/String1.py
+++ CodingBatPython/String1.py
@@ -2,6 +2,12 @@
 # hello_name
 def hello_name(name):
   return ("Hello " + name + "!")
+
 # non_start
 def non_start(a, b):
   return a[1:] + b[1:]
+
+# first_half
+def first_half(str):
+  return str[0:(len(str))/2]
+
