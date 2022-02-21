0769a2ddb6bb4bb971a6af7ca5ebea1890a09b5e
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-21 13:35:56 -0500

    Create Basic1.py

---
+++ PythonPractice/Basic1.py
@@ -0,0 +1,19 @@
+# Interpreted, interactive, object-oriented, high-level
+print("Hello, world!") # how to print to the console
+Manpower = 22
+manpower = 22
+# Python is case sensitive, so this is two different variables
+
+# Code blocks are defined by indentation, not curly braces like Java
+if True:
+    print "True"
+else:
+    print "False"
+   
+word = 'Single quotations are allowed for string literals'
+word1 = "As are double quotations"
+word2 = """Or use triple quotes for multi-line
+        comments"""
+        
+# There is a lot to Python, but ultimately it is similar to Java in many ways
+
