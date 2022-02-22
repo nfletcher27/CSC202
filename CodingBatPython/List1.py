ef2a36ff51ac13c4a9e033b9dcb729cb6013bd00
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-21 21:14:21 -0500

    Update List1.py

--- CodingBatPython/List1.py
+++ CodingBatPython/List1.py
@@ -6,3 +6,10 @@ def first_last6(nums):
     return True
   else:
     return False
+
+  # common_end
+  def common_end(a, b):
+  if a[0] == b[0] or a[len(a)-1] == b[len(b)-1]:
+    return True
+  else:
+    return False
