7cb1617293501a808b7ab320056ff5ca8d0c4e24
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-22 19:59:34 -0500

    Update InsertionSort.java

--- Algorithms/InsertionSort.java
+++ Algorithms/InsertionSort.java
@@ -8,10 +8,10 @@ public void insertionSort(int arr[]) {
 		int key = arr[i];
 		int j = i - 1;
 		
-		while(j >= 0 && arr[j] > key) {
-			arr[j+1] = arr[j];
-			j = j - 1;
-		}
-		arr[j+1] = key;
+	while(j >= 0 && arr[j] > key) {
+		arr[j+1] = arr[j];
+		j = j - 1;
+	}
+	arr[j+1] = key;
 	}
 }
