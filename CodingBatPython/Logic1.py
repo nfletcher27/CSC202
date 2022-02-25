5bb036a4e45e6b9bf82ae9d29554c71e41196db3
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-24 19:07:26 -0500

    Update Logic1.py

--- CodingBatPython/Logic1.py
+++ CodingBatPython/Logic1.py
@@ -7,7 +7,7 @@ def cigar_party(cigars, is_weekend):
   else:
     return cigars > 39 and cigars < 61
   
-  # love6
+# love6
   def love6(a, b):
   if a == 6 or b == 6:
     return True
@@ -16,3 +16,19 @@ def cigar_party(cigars, is_weekend):
   else:
     return False
 
+# caught_speeding
+def caught_speeding(speed, is_birthday):
+  if is_birthday:
+    if speed <= 65:
+      return 0
+    elif speed > 65 and speed < 86:
+      return 1
+    else:
+      return 2
+  elif speed < 61:
+    return 0
+  elif speed > 60 and speed < 81:
+    return 1
+  else:
+    return 2
+  
