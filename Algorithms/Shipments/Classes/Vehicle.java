4bf3a61a67d9c2b01a2788633792e8c73022b6b1
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-01 21:48:26 -0500

    Create Vehicle.java

---
+++ Algorithms/Shipments/Classes/Vehicle.java
@@ -0,0 +1,29 @@
+public class Vehicle {
+	
+	private double weightCapacity;
+	private String plate;
+	private String driverName;
+	
+	public Vehicle(double weightCapacity, String plate, String driverName) {
+		super();
+		this.weightCapacity = weightCapacity;
+		this.plate = plate;
+		this.driverName = driverName;
+	}
+
+	public double getWeightCapacity() {
+		return weightCapacity;
+	}
+
+	public String getPlate() {
+		return plate;
+	}
+
+	public String getDriverName() {
+		return driverName;
+	}
+
+	public void setDriverName(String driverName) {
+		this.driverName = driverName;
+	}
+}
