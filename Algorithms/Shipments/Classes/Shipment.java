563ca5a612a831dbe5f4dbe087cc21eb67e3936b
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-01 21:48:38 -0500

    Create Shipment.java

---
+++ Algorithms/Shipments/Classes/Shipment.java
@@ -0,0 +1,35 @@
+public class Shipment {
+
+	private double weight;
+	private long barcodeNumber;
+	private int fromPostalCode;
+	private int toPostalCode;
+	
+	public Shipment(double weight, long barcodeNumber, int fromPostalCode, int toPostalCode) {
+		super();
+		this.weight = weight;
+		this.barcodeNumber = barcodeNumber;
+		this.fromPostalCode = fromPostalCode;
+		this.toPostalCode = toPostalCode;
+	}
+
+	public int getFromPostalCode() {
+		return fromPostalCode;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public long getBarcodeNumber() {
+		return barcodeNumber;
+	}
+
+	public int getToPostalCode() {
+		return toPostalCode;
+	}
+	
+	public String getInformation() {
+		return "Weight: " + this.weight + " Barcode: " + this.barcodeNumber;
+	}
+}
