c1cee3bfe177af37f2f612c45fcce47a4217451d
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-05 16:55:46 -0500

    Update Warmup1.py

--- CodingBatPython/Warmup1.py
+++ CodingBatPython/Warmup1.py
@@ -38,3 +38,10 @@ def pos_neg(a, b, negative):
 # makes10
 def makes10(a, b):
   return (a == 10 or b == 10) or (a+b == 10)
+
+# sum_double
+def sum_double(a, b):
+  if a == b:
+    return (a+b)*2
+  else:
+    return a+b
