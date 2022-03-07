545166c51bc2594020c3a8b82ec18765a9dada00
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-07 16:22:19 -0500

    Update JavaRecursion.java

--- JavaRecursion.java
+++ JavaRecursion.java
@@ -2,3 +2,9 @@
 public int bunnyEars(int bunnies) {
   return bunnies + bunnies;
 }
+
+// fibonacci
+public int fibonacci(int n) {
+  if(n <= 1) return n; // base case for 0 and 1
+  return fibonacci(n-1) + fibonacci(n-2); // takes the two prior numbers of n to add (which is how fibo works), and does that same function until it reaches 1
+}
