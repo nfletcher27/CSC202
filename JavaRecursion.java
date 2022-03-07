a95fa8ac39028ddc058c195cfef32efafe27b215
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-07 16:43:08 -0500

    Update JavaRecursion.java

--- JavaRecursion.java
+++ JavaRecursion.java
@@ -8,3 +8,9 @@ public int fibonacci(int n) {
   if(n <= 1) return n; // base case for 0 and 1
   return fibonacci(n-1) + fibonacci(n-2); // takes the two prior numbers of n to add (which is how fibo works), and does that same function until it reaches 1
 }
+
+// factorial
+public int factorial(int n) {
+  if(n <= 2) return n;
+  return n * factorial(n-1);
+}
