628d7d6bd646bc1c855fd0aff5a7c4fdb8f5c204
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-08 00:09:29 -0500

    Update JavaRecursion.java

--- JavaRecursion.java
+++ JavaRecursion.java
@@ -14,3 +14,9 @@ public int factorial(int n) {
   if(n <= 2) return n;
   return n * factorial(n-1);
 }
+
+// triangle
+public int triangle(int rows) {
+  if(rows <= 1) return rows; // base case, 0 and 1 will return themselves
+  return rows + triangle(rows-1); // the triangle formula is simply itself + the sum of all rows prior, by calling the same method of n-1 we do this logic recursively
+}
