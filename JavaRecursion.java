c0d1999202a37f6ce93c1d0afc515ae2a0197b23
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-08 00:18:17 -0500

    Update JavaRecursion.java

--- JavaRecursion.java
+++ JavaRecursion.java
@@ -20,3 +20,11 @@ public int triangle(int rows) {
   if(rows <= 1) return rows; // base case, 0 and 1 will return themselves
   return rows + triangle(rows-1); // the triangle formula is simply itself + the sum of all rows prior, by calling the same method of n-1 we do this logic recursively
 }
+
+// bunnyEars2
+public int bunnyEars2(int bunnies) {
+  if(bunnies == 0) return 0;
+  if(bunnies == 1) return 2;
+  if(bunnies % 2 == 0) return bunnyEars2(bunnies-1) + 3;
+  return bunnyEars2(bunnies-1) + 2;
+}
