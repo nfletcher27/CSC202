0dbb731765eb1225550d56d0aaf40b882a7e0c91
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-09 16:51:53 -0500

    Update List1.py

--- CodingBatPython/List1.py
+++ CodingBatPython/List1.py
@@ -7,9 +7,16 @@ def first_last6(nums):
   else:
     return False
 
-  # common_end
+ # common_end
   def common_end(a, b):
   if a[0] == b[0] or a[len(a)-1] == b[len(b)-1]:
     return True
   else:
     return False
+
+# reverse3
+def reverse3(nums):
+  swap = nums[0]
+  nums[0] = nums[2]
+  nums[2] = swap
+  return nums
