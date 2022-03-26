2f4b18d122f49fac429ddaaed1ff8287298b0dbd
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-26 13:52:24 -0400

    Update Logic1.py

--- CodingBatPython/Logic1.py
+++ CodingBatPython/Logic1.py
@@ -54,3 +54,10 @@ def sorta_sum(a, b):
     return 20
   else:
     return a + b
+
+# squirrel_play
+def squirrel_play(temp, is_summer):
+  if is_summer:
+    return temp >= 60 and temp <= 100
+  else:
+    return temp >= 60 and temp <= 90
