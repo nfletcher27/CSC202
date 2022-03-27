3bebc4681c18926188692b9e12ceddac8dbee398
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-27 13:04:52 -0400

    Update Logic2.py

--- CodingBatPython/Logic2.py
+++ CodingBatPython/Logic2.py
@@ -1,1 +1,13 @@
 # Logic2 problems
+
+# lone_sum
+def lone_sum(a, b, c):
+  if(a==b and b==c):
+    return 0
+  if(a==b):
+    return c
+  if(b==c):
+    return a
+  if(c==a):
+    return b
+  return a+b+c
