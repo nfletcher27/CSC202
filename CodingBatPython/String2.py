041099a8efc24d9ce88c2f6dc7cbced34997cb02
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-27 13:20:02 -0400

    Update String2.py

--- CodingBatPython/String2.py
+++ CodingBatPython/String2.py
@@ -1,1 +1,15 @@
 # String2 problems
+
+# cat_dog
+def cat_dog(str):
+  cCount = 0
+  dCount = 0
+  for i in range(len(str)):
+    if(str[i:i+3] == "cat"):
+      cCount += 1
+    if(str[i:i+3] == "dog"):
+      dCount += 1
+      
+  if(cCount == dCount):
+    return True
+  return False
