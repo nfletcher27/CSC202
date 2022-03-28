38baaf0bb40f6678d801cef6f38211a59858cc8c
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-27 21:46:11 -0400

    Create excel.py

---
+++ excel.py
@@ -0,0 +1,33 @@
+# uses openpyxl to take an .xlsx file and iterate through it, taking the values and altering the sheet into a new one with added features
+# creates new row and a bar graph showing the data values in a visual representation
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
+
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+    cell = sheet.cell(1, 1)
+
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+
+    values = Reference(sheet,
+                       min_row=2,
+                       max_row=sheet.max_row,
+                       min_col=4,
+                       max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+
+    wb.save(filename)
+
+
+file = input("What is the name of your file? (include .xlsx)")
+process_workbook(file)
+print("You're file has been overwritten.")
