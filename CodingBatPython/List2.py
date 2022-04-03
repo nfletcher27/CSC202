8cb9e4a46668ef767a7051a9f97f77c3933a5f7c
Nesta R. Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-04-02 22:24:38 -0400

    Update List2.py

--- CodingBatPython/List2.py
+++ CodingBatPython/List2.py
@@ -1,1 +1,11 @@
 # List2 problems
+
+# count_Evens
+def count_evens(nums):
+  if len(nums) == 0:
+    return 0
+  count = 0
+  for i in range(len(nums)):
+    if nums[i] % 2 == 0:
+      count += 1
+  return count
