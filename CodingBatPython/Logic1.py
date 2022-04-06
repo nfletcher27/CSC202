fbf029e13e76ee1a6181ef6a02e36b5c6a6eafd3
Nesta R. Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-04-05 23:23:13 -0400

    Update Logic1.py

--- CodingBatPython/Logic1.py
+++ CodingBatPython/Logic1.py
@@ -61,3 +61,17 @@ def squirrel_play(temp, is_summer):
     return temp >= 60 and temp <= 100
   else:
     return temp >= 60 and temp <= 90
+
+  
+# alarmClock
+def alarm_clock(day, vacation):
+  if vacation:
+    if day != 0 and day != 6:
+      return '10:00'
+    else:
+      return 'off'
+  else:
+    if day != 0 and day != 6:
+      return '7:00'
+    else:
+      return '10:00'
