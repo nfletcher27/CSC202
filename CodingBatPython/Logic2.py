612a45878ff551076c70f445cede2001ededfd24
Nesta R. Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-04-24 21:00:29 -0400

    Update Logic2.py

--- CodingBatPython/Logic2.py
+++ CodingBatPython/Logic2.py
@@ -21,3 +21,15 @@ def lucky_sum(a, b, c):
   if c == 13:
     return a + b
   return a + b + c
+
+# no_teens
+def no_teen_sum(a, b, c):
+  return fix_teen(a) + fix_teen(b) + fix_teen(c) 
+def fix_teen(n):
+  if n > 10 and n < 20:
+    if n == 15 or n == 16:
+      return n
+    else:
+      return 0
+  else:
+    return n
