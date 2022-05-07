bf6bcb1a7e0477adac8f42bdd4464aacdd56f7e8
Nesta <89424122+nfletcher27@users.noreply.github.com>
2022-05-07 15:54:50 -0400

    Update String2.py

--- CodingBatPython/String2.py
+++ CodingBatPython/String2.py
@@ -13,3 +13,12 @@ def cat_dog(str):
   if(cCount == dCount):
     return True
   return False
+
+# counthi
+def count_hi(str):
+  counter = 0
+  for i in range(len(str)):
+    if str[i:i+2] == 'hi':
+      counter += 1
+
+  return counter
